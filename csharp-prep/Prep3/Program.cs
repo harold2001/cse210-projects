3ad32152000261fddeeed31c58a3bfb4bcfa2658
harold2001 <hcarazasnires@gmail.com>
2024-01-13 13:04:21 -0500

    prep3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int attempts = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You made {attempts} attempts.");
+            }
+
+        }
+
     }
 }
