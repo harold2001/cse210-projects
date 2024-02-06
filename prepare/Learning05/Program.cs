0d0739f03577ec807f151ce65d6471612af9c5e3
harold2001 <hcarazasnires@gmail.com>
2024-02-06 00:06:23 -0500

    Learning 05 finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Shape shape = new("red");
+
+        Square square = new("blue", 5);
+        // Console.WriteLine(square.GetColor());
+        // Console.WriteLine(square.GetArea());
+        Circle circle = new("green", 3);
+        Rectangle rectangle = new("yellow", 3, 4);
+
+
+        List<Shape> shapes = new()
+        {
+            shape,
+            square,
+            circle,
+            rectangle
+        };
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine(s.GetType());
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+            Console.WriteLine();
+        }
     }
 }
---
+++ prepare/Learning05/Rectangle.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Rectangle : Shape
+{
+    private readonly double _length;
+    private readonly double _width;
+
+    public Rectangle(string color, double length, double width) : base(color)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
